11937451f4ab8a4b508c18c1c1f0e6a39c076383
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2024-02-04 19:34:25 +0200

    initial commit

---
+++ src/main/java/com/sevice/provider/serviceproviderresourceserver/ServiceProviderResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.sevice.provider.serviceproviderresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderResourceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sevice/provider/serviceproviderresourceserver/ServiceProviderResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevice.provider.serviceproviderresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
